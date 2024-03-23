3c914eab336c31d7cafc579e3152b8dfa18bdec8
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-03-23 22:57:24 +0100

    feat: modified method return

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -14,11 +16,27 @@ class ProductController extends AbstractController
     ) {
     }
 
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/product', name: 'app_product', methods: ['GET'])]
+    public function index(): Response | JsonResponse
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+        $product = $this->client->request('get', 'http://localhost:8800/product');
+
+        return $this->json($product, 200);
+
+        // return $this->render('product/index.html.twig', [
+        //     'controller_name' => 'ProductController',
+        // ]);
+    }
+
+    #[Route('/product/{productslug}', name: 'app_product_show', methods: ['GET'])]
+    public function show($productslug): Response | JsonResponse
+    {
+        $product_detail = $this->client->request('get', 'http://localhost:8800/product/' . $productslug, []);
+
+        return $this->json($product_detail, 200);
+
+        // return $this->render('product/index.html.twig', [
+        //     'controller_name' => 'ProductController',
+        // ]);
     }
 }
